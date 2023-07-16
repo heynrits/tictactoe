9137e4310b72807b54791231b86b581211486e1b
John Heinrich Austria <jbaustria@up.edu.ph>
2023-07-17 01:44:22 +0800

    setup: Configure database connection string for deployment

--- tictactoe-backend/database/db.js
+++ tictactoe-backend/database/db.js
@@ -3,8 +3,17 @@ const mongoose = require('mongoose');
 const connect = async () => {
     const DB_HOST = process.env.DB_HOST || 'mongodb://127.0.0.1:27017/';
     const DB_NAME = process.env.DB_NAME || 'test';
+
+    let dbConnectionStr = `${DB_HOST}${DB_NAME}`;
+    if (ENVIRONMENT === 'production') {
+        const DB_CLUSTER = process.env.DB_CLUSTER;
+        const DB_NAME = process.env.DB_NAME;
+        const DB_USERNAME = process.env.DB_USERNAME;
+        const DB_PASSWORD = process.env.DB_PASSWORD;
+        dbConnectionStr = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@${DB_CLUSTER}.fcosuu8.mongodb.net/${DB_NAME}?retryWrites=true&w=majority`
+    }
     
-    await mongoose.connect(`${DB_HOST}${DB_NAME}`);
+    await mongoose.connect(dbConnectionStr);
 };
 
 exports.connect = connect;
