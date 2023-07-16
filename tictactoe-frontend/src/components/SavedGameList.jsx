c68b3a537978b7a86e204c66a3eb2a6b7bca8cab
John Heinrich Austria <jbaustria@up.edu.ph>
2023-07-16 20:53:29 +0800

    feat: [Client] Implement deleting of saved game data

--- tictactoe-frontend/src/components/SavedGameList.jsx
+++ tictactoe-frontend/src/components/SavedGameList.jsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 import dayjs from "dayjs"
-import { useEffect, useState } from "react"
-import { SlClock, SlTrophy, SlUser } from "react-icons/sl"
+import { useEffect, useRef, useState } from "react"
+import { SlClock, SlTrophy, SlUser, SlTrash } from "react-icons/sl"
 import { useNavigate } from "react-router-dom"
 import LoadingIndicator from "./LoadingIndicator"
 
@@ -10,18 +10,28 @@ function SavedGameList() {
     const [loading, setLoading] = useState(true)
 
     useEffect(() => {
+        loadGames()
+    }, [])
+
+    const handleDelete = (id) => {
+        axios.delete(`/games/${id}`)
+            .then(() => loadGames())
+    }
+
+    function loadGames() {
+        setLoading(true)
         axios.get('/games').then(res => {
             setGames(res.data)
             setLoading(false)
         })
-    }, [])
+    }
 
     return (
         <div className="my-8 w-10/12 max-w-xl text-center animate-slide-right">
 
             <div className="font-chelsea mb-4">Load Game</div>
 
-            <div className="min-h-[30vh] max-h-[450px] overflow-y-scroll py-8 border-y-2 border-gray-200 flex items-center flex-col justify-center gap-4">
+            <div className={`min-h-[30vh] max-h-[450px] py-8 overflow-y-scroll border-y-2 border-gray-200 flex items-center flex-col gap-4 ${loading ? 'justify-center' : 'justify-start'}`}>
             {
                     loading ? (
                         <LoadingIndicator color='white' text='Loading...' />
@@ -29,7 +39,7 @@ function SavedGameList() {
                         <>
                             {
                                 games.length > 0 ? (
-                                    games.map((game, i) => <ListItem key={game._id} game={game} isFirst={i === 0} />)
+                                    games.map((game, i) => <ListItem key={game._id} game={game} onDelete={handleDelete} />)
                                 ) : <span className="text-sm">No saved games yet.</span>
                             }
                         </>
@@ -44,24 +54,64 @@ function SavedGameList() {
 /**
  * Displays game information such as player names, win counts, and draw counts
  */
-function ListItem({ game, isFirst }) {
+function ListItem({ game, onDelete }) {
     const navigate = useNavigate()
 
+    const [showMenu, setShowMenu] = useState(false)
+    const menuRef = useRef(null)
+
+    useEffect(() => {
+        const handleOutsideClick = (event) => {
+            if (menuRef.current && !menuRef.current.contains(event.target)) {
+                setShowMenu(false)
+                console.log('outside')
+            }
+        }
+
+        document.addEventListener('click', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        }
+    }, [])
+
+    const handleContextMenu = (e) => {
+        e.preventDefault();
+        setShowMenu(true);
+      };
+    
+      const handleDelete = () => {
+        onDelete(game._id);
+        setShowMenu(false);
+      };
+
     return (
-        <div className={`w-full ${isFirst ? 'mt-8' : ''}`}>
+        <div className="w-full">
             <div className="flex items-center gap-2">
                 <SlClock /> {dayjs(game.updatedAt).format("MMM D, YYYY h:mm A")}
             </div>
 
-            <div className="bg-white text-slate-blue border-4 border-white py-1 px-3 mt-2 rounded-2xl flex justify-between font-chelsea hover:cursor-pointer hover:border-4 hover:border-sand-yellow" onClick={() => navigate(`/play?gameId=${game._id}`)}>
-                <LiPlayerInfo mark="O" name={game.p1.name} wins={game.p1.winCount} />
-                <div>
-                    <span className="text-2xl">vs</span>
-                    <div className="flex items-center gap-1 relative">
-                        <span className="text-lg absolute ml-1 opacity-80">/</span><SlTrophy /> {game.drawCount}
+            <div className="relative" onContextMenu={handleContextMenu}>
+                <div className="bg-white text-slate-blue border-4 border-white py-1 px-3 mt-2 rounded-2xl flex justify-between font-chelsea hover:cursor-pointer hover:border-4 hover:border-sand-yellow" onClick={() => navigate(`/play?gameId=${game._id}`)}>
+                    <LiPlayerInfo mark="O" name={game.p1.name} wins={game.p1.winCount} />
+                    <div>
+                        <span className="text-2xl">vs</span>
+                        <div className="flex items-center gap-1 relative">
+                            <span className="text-lg absolute ml-1 opacity-80">/</span><SlTrophy /> {game.drawCount}
+                        </div>
                     </div>
+                    <LiPlayerInfo mark="X" name={game.p2.name} wins={game.p2.winCount} />
                 </div>
-                <LiPlayerInfo mark="X" name={game.p2.name} wins={game.p2.winCount} />
+                {showMenu && (
+                    <div className="absolute bottom-[-50%] right-0 w-40 bg-white border border-gray-300 shadow" ref={menuRef}>
+                    <button
+                        className="flex items-center gap-2 w-full text-left px-4 py-2 hover:bg-gray-100 text-pale-red font-sans"
+                        onClick={handleDelete}
+                    >
+                        <SlTrash /> Delete
+                    </button>
+                    </div>
+                )}
             </div>
         </div>
     )
@@ -74,8 +124,8 @@ function LiPlayerInfo({ mark, name, wins }) {
     return (
         <div className="w-[150px] flex items-center gap-4">
             <span className="text-4xl">{mark}</span>
-            <div>
-                <div className="flex items-center gap-2">
+            <div className="">
+                <div className="flex items-center gap-2 truncate">
                     <SlUser /> {name}
                 </div>
                 <div className="flex items-center gap-2">
